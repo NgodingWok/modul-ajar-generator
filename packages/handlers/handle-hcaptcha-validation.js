cf16065abff41894cd732c6ea2d2e585408fbfac
GTPSHAX <rafiehasannudin@gmail.com>
2026-05-06 01:45:08 +0700

    feat: rearrange hCaptcha validation logic for improved error handling

--- packages/handlers/handle-hcaptcha-validation.js
+++ packages/handlers/handle-hcaptcha-validation.js
@@ -6,8 +6,17 @@ import { verify } from 'hcaptcha'
  * @returns {Promise<boolean>} - A promise that resolves to true if the hCaptcha validation is successful, or false otherwise.
  */
 const handleHCaptchaValidation = async (body) => {
-  const { 'h-captcha-response': hCaptchaResponse } = body
   const secret = String(process.env.HCAPTCHA_SECRET_KEY)
+  if (!secret) {
+    console.warn('Bypassing hCaptcha validation because HCAPTCHA_SECRET_KEY is not set in environment variables.')
+    return true
+  }
+
+  const { 'h-captcha-response': hCaptchaResponse } = body
+  if (!hCaptchaResponse) {
+    console.warn('hCaptcha response token is missing in the request body.')
+    return false
+  }
 
   try {
     const result = await verify(secret, hCaptchaResponse)
